e0d45f84100d412c1a0514f8c365932fab2f764d
richelbilderbeek <richel@richelbilderbeek.nl>
2022-01-18 10:26:22 +0100

    No .map file is needed

--- get_admixture.sh
+++ get_admixture.sh
@@ -13,17 +13,27 @@
 #
 if [ ! -f admixture_linux-1.3.0.tar.gz ]
 then
+    echo "Download ADMIXTURE"
     wget https://dalexander.github.io/admixture/binaries/admixture_linux-1.3.0.tar.gz
 fi
+
+echo "Unpack ADMIXTURE"
 tar -xf admixture_linux-1.3.0.tar.gz
+
+echo "Create a symlink to ADMIXTURE"
 ln -s dist/admixture_linux-1.3.0/admixture
 
 if [ ! -f hapmap3-files.tar.gz ]
 then
+    echo "Download ADMIXTURE example files"
     wget https://dalexander.github.io/admixture/hapmap3-files.tar.gz
 fi
 
+echo "Unpack the ADMIXTURE example files"
 tar -xf hapmap3-files.tar.gz
 
-# Remove these -probably- backup files
+echo "Remove the -probably- backup files"
 rm ._hapmap3.*
+
+echo "Remove the .map file, as it is not used"
+rm hapmap3.map
